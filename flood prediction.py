f942449996fabb94954f685cac024743da5bc18d
Tanmayshinde-bits <65349565+Tanmayshinde-bits@users.noreply.github.com>
2021-07-12 20:32:47 +0530

    Add files via upload

---
+++ flood prediction.py
@@ -0,0 +1,18 @@
+{
+    "Ultrasonic sensor data": [
+        "1", [,
+            "date": 30 - 06 - 2021
+"time": 3:30
+"data": 10
+kHz
+]
+],
+2,
+3
+],
+"Temperature sensor data": [
+    1,
+    2,
+    3
+],
+"null": null,
